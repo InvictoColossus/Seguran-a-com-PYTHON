7a72e3289360dfe1ccdadd8d4439d5bd69d023e3
Gedalias Tarcisio <75184726+InvictoColossus@users.noreply.github.com>
2021-06-21 13:11:35 -0300

    Update ClienteTcp.py

--- ClienteTcp.py
+++ ClienteTcp.py
@@ -1,13 +1,13 @@
-import  socket   # faz o relacionamento da placa de rede e o SO
-import sys     #fornece o acesso algumas variáveis e funções que tem no python
+import  socket   
+import sys      
 
 def main():
-    try:          #pega da biblioteca socket o metodo socket AF_INET mostra q vamos utilizar o protocolo ip
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)  #SOCK_STREAM que passara o protocolo tcp
-    except socket.error as erro:  #0 passa o protocolo que fará a comunicação com o servidor o tcp
+    try:          
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)  
+    except socket.error as erro:  
         print('A conexão falhou !!!')
         print('Erro: {}'.format(erro))
-        sys.exit()      #sai do programa de der erro
+        sys.exit()      
 
     print('Socket criado com SUCESSO!!')
 
@@ -17,7 +17,7 @@ def main():
     try:
         s.connect((hostAlvo,int(portaAlvo)))
         print("Cliente TCP conectado com Sucesso"+ hostAlvo + 'e na porta: ' + portaAlvo)
-        s.shutdown(2)      #Finaliza a Conexão
+        s.shutdown(2)      
     except socket.error as e:
         print('Não foi possível conectar no host:'+ hostAlvo + 'e na porta:' + portaAlvo)
         print('Error {}'.format(e))
